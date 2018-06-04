2091ee1322fc6a3042845b97460f6c410f03333c
hemantgrovo <hdhiman@grovo.com>
2018-06-04 16:00:30 -0700

    Install mycrypt

--- grovo.sh
+++ grovo.sh
@@ -7,15 +7,24 @@ brew install liquibase
 #install glide for golang package
 brew install glide
 
-#install mcrypt for ssologin
-brew install homebrew/php/php71-mcrypt
-
+#install php
 brew install homebrew/php/php71 --with-postgresql --with-phpdbg
 #Please install xcode before this as xdebug requires xcode and xdebug is used by codecov
 brew install git-flow php7x-xdebug yarn
+
+#install mcrypt
+#homebrew /usr/local is not writable in high sierra (sometimes include folder doesn't exist)
+cd /usr/local
+sudo mkdir include
+sudo chown -R $(whoami) $(brew --prefix)/include
+
+#install libmcrypt
+brew install libmcrypt
+
+#once downloaded do the below step
+sudo pecl install mcrypt-1.0.0
 
 #Install composer package to install services dependencies
 brew install composer
 
 brew install git-flow homebrew/php/php71-xdebug yarn
-
